e3acabdae2d258055dedb733e080ff2a2ef0043a
Drew <drewwebs@gmail.com>
2020-12-17 00:22:03 -0500

    Day 12 added

--- day_12/day_twelve.rb
+++ day_12/day_twelve.rb
@@ -1,9 +1,35 @@
+DIRECTIONS = %w[north, south, east, west]
 
 def get_manhattan_distance
     input = File.open('input', 'r')
 
+    dirsHash = Hash.new(0)
+    dir = 90
+
     input.each do |line|
-        direction, distance = line.split(" ")
+        direction = line[0]
+        distance = line[1..-1]
         
+        if %w[north, south, east, west].include?(direction)
+            %w[north, south].include?(direction) ? dirsHash["netNorth"] += distance : dirsHash["netEast"] += distance
+        elsif %[left, right].include?(direction)
+            dir == 'left' ? dir -= distance : dir += distance
+        else
+            dir = direction / 4
+            direction = DIRECTIONS[dir]
+            case direction
+            when 'north'
+                dirsHash['netNorth'] += distance
+            when 'east'
+                dirsHash['netEast'] += distance
+            when 'south'
+                dirsHash['netNorth'] -= distance
+            when 'west'
+                dirsHash['netEast'] -= distance
+            end
+        end
     end
+    dirsHash['netNorth'] + dirsHash['netEast']
 end
+
+p get_manhattan_distance
